dabf50807857b1585469502e5aea8e327a8c0f1c
hannacheikh11 <hanna.cheikhmalaaynine@gmail.com>
2021-01-04 15:34:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import React, { Component } from "react"
+function App() {
+
+  
+ 
+
+  return (
+    <div className="grid-container">
+  <header>
+              <a href="/">Order</a>
+               
+              </header>
+              <main>
+                Product List
+             </main> 
+               <footer>
+                 All rignt
+               </footer>
+             
+             
+              </div>
+  );
+}
+
+export default App;
